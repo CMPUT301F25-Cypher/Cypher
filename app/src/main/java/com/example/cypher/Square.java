64647df712ac021b2f792670d3ac5f10bab60442
Kanav Dutta <kanavdatta04@gmail.com>
2025-09-23 17:59:26 -0600

    ADDED SQUARE CLASS EXTENDING SHAPE

---
+++ app/src/main/java/com/example/cypher/Square.java
@@ -0,0 +1,17 @@
+public class Circle extends Shape {
+    private int radius;
+    
+    public Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    
+    public int getRadius() {
+        return radius;
+    }
+    
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
